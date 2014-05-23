6a7b3b849e087d7e0f62a16a37ad4be6947a5072
Raúl Jiménez <jimenezjrs@gmail.com>
2014-05-23 16:08:32 -0500

    Finished A section questions

--- app/scripts/directives/question.js
+++ app/scripts/directives/question.js
@@ -9,25 +9,25 @@ angular.module('questionModule', [])
       console.log(fieldType);
       var template = '';
       template += '<pre ng-if="code">{{code | json}}</pre>';
-      template += '<div class="form-group"><label for="{{title}}" class="col-sm-3 control-label">{{title}}</label>';
+      template += '<h3>{{title}}</h3>';
+      template += '<h4>{{help}}</h4>';
+
 
       switch(fieldType){
       case 'text':
-        template += '<div class="col-sm-9"><input class="form-control" type="text" ng-model="body.value"></div>';
+        template += '<input class="form-control" type="text" ng-model="body.value">';
         break;
       case 'number':
-        template += '<div class="col-sm-9"><input class="form-control" type="number" ng-model="body.value"></div>';
+        template += '<input class="form-control" type="number" ng-model="body.value">';
         break;
       case 'radio':
-        template += '<div class="col-sm-9"><div ng-repeat="opt in body.options" class="row-fluid"><label><input type="radio" name="radio{{outerIndex}}" ng-value="opt" ng-model="body.value" />&nbsp;<span ng-bind="opt"></span></label></div></div>';
+        template += '<div ng-repeat="opt in body.options" class="row-fluid"><label><input type="radio" name="radio{{outerIndex}}" ng-value="opt" ng-model="body.value" />&nbsp;<span ng-bind="opt"></span></label></div>';
         break;
       case 'checkbox':
-        template += '<div class="col-sm-9"><div ng-repeat="opt in body.options" class="row-fluid"><label><input type="checkbox" name="checkbox{{outerIndex}}" ng-value="opt" ng-model="opt.checked" />&nbsp;<span ng-bind="opt.label"></span></label></div></div>';
+        template += '<div ng-repeat="opt in body.options" class="row-fluid"><label><input type="checkbox" name="checkbox{{outerIndex}}" ng-value="opt" ng-model="opt.checked" />&nbsp;<span ng-bind="opt.label"></span></label></div>';
         break;
 
       }
-      template += '</div>';
-
       return template;
     };
 
