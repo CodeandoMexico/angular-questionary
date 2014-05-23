924377e30d2aab4dc80198d1599e86f70c7c1315
Raúl Jiménez <jimenezjrs@gmail.com>
2014-05-23 17:27:48 -0500

    Finish translating to json the answers

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -125,7 +125,16 @@ angular.module('questionaryApp')
             }
           },
           {
-            title  : 'Del total de personas que trabajan en la empresa, ¿cuantas son familiares de los dueños  (incluye: padres, hijos, abuelos, hermanos, tíos, primos, sobrinos, cuñados)?',
+            title  : '¿Cuántos dueños (socios) tiene este negocio?',
+            // help   : 'Escribe el valor de la respuesta',
+            type   : 'number',
+            body   : {
+              value  : null,
+            }
+          },
+
+          {
+            title  : 'Del total de personas que trabajan en la empresa, ¿cuántos son familiares de los dueños  (incluye: padres, hijos, abuelos, hermanos, tíos, primos, sobrinos, cuñados)?',
             // help   : 'Escribe el valor de la respuesta',
             type   : 'number',
             body   : {
@@ -133,12 +142,12 @@ angular.module('questionaryApp')
             }
           },
           {
-            title    : '¿Resides en el D.F.?',
+            title    : '¿El producto/servicio que se ofrece es?',
             // help     : 'Selecciona uno de los valores',
             type     : 'radio',
             body     : {
               value    : 'Es nuevo y distinto a lo que existe y/o el proceso de elaboración/comercialización es innovador',
-              options  : ['Es nuevo y distinto a lo que existe y/o el proceso de elaboración/comercialización es innovador', 'En algunas características son diferentes a los de mi competencia o lo ofrezco a personas que no lo tienen', 'Ya existe y es ofrecido por otros']
+              options  : ['Es nuevo y distinto a lo que existe y/o el proceso de elaboración/comercialización es innovador', 'En algunas características son diferentes a los de mi competencia o lo ofrezco a personas que no lo tienen', 'Ya existe y es ofrecido por otros' ]
             }
           },
           {
@@ -148,14 +157,6 @@ angular.module('questionaryApp')
             body     : {
               value    : 'Aceptar el empleo y dejar de trabajar en mi empresa',
               options  : ['Aceptar el empleo y dejar de trabajar en mi empresa', 'No aceptar el empleo y seguir con mi empresa']
-            }
-          },
-          {
-            title  : '¿Cuanto tiempo lleva este proyecto/negocio en operación?',
-            // help   : 'Escribe el valor de la respuesta',
-            type   : 'number',
-            body   : {
-              value  : null,
             }
           },
         ],
@@ -302,6 +303,147 @@ angular.module('questionaryApp')
                 ]
             }
           }
+        ]
+      },
+      {
+        identifier : '2.C.1',
+        grouped    : true,
+        questions : [
+          {
+            title  : '[ORDINAL] La razón principal por la que llevo a cabo este proyecto es porque...',
+            help   : 'Escribe el valor de la respuesta',
+            type   : 'checkbox',
+            body   : {
+              options: [
+                { label: 'No hay suficientes oportunidades laborales para encontrar un empleo', checked: true },
+                { label: 'Prefiero emprender mi propio negocio que ser un empleado', checked: true },
+                { label: 'Quiero generar un impacto positivo en la sociedad y/o medio ambiente', checked: true },
+                { label: 'Quiero desarrollar mi creatividad', checked: true },
+                { label: 'Hago lo que más me gusta hacer', checked: true },
+                { label: 'Tengo algo innovador que es (o será) rentable en el mercado', checked: true },
+              ]
+            }
+          },
+          {
+            title  : '[ORDINAL] El principal objetivo de mi empresa será...',
+            help   : 'Escribe el valor de la respuesta',
+            type   : 'checkbox',
+            body   : {
+              options: [
+                { label: 'Obtener ingresos para solventar los gastos básicos personales/familiares', checked: true },
+                { label: 'Tener mi propio negocio sin depender de un tercero', checked: true },
+                { label: 'Realizar la actividad que más me gusta hacer y vivir de ello', checked: true },
+                { label: 'Exponer un proyecto artístico', checked: true },
+                { label: 'Generar un impacto positivo en la población y/o el medio ambiente', checked: true },
+                { label: 'Realizar un proyecto empresarial innovador y exitoso', checked: true },
+              ]
+            }
+          },
+          {
+            title    : '¿Cuál de las siguientes frases describe mejor tu situación laboral-empresarial actual?',
+            // help     : 'Selecciona uno de los valores',
+            type     : 'radio',
+            body     : {
+              value    : 'NO tengo empleo y NO tengo otro negocio',
+              options  : [
+                'NO tengo empleo y NO tengo otro negocio',
+                'SÍ tengo empleo y NO tengo otro negocio',
+                'NO tengo empleo y SÍ tengo otro negocio',
+                'SÍ tengo empleo y SÍ tengo otro negocio'
+              ]
+            }
+          },
+          {
+            title    : 'Imagina que ya tienes tu negocio con el cual las ganacias son igual a 100, si te ofrecieran un empleo con un salario de 105 ¿qué preferirías?',
+            // help     : 'Selecciona uno de los valores',
+            type     : 'radio',
+            body     : {
+              value    : 'Aceptar el empleo y dejar de trabajar en mi empresa',
+              options  : ['Aceptar el empleo y dejar de trabajar en mi empresa', 'No aceptar el empleo y seguir con mi empresa']
+            }
+          },
+          {
+            title    : '¿Tu negocio pertenecerá a alguno de los siguientes sectores?',
+            // help     : 'Selecciona uno de los valores',
+            type     : 'radio',
+            body     : {
+              value    : 'Industrias manufactureras',
+              options  : [
+                'Industrias manufactureras',
+                'Comercio',
+                'Preparación de alimentos y bebidas (restaurantes, puestos y similares) y hoteles',
+                'Servicios profesionales, técnicos, corporativos, financieros, inmobiliarios, educativos, médicos, de apoyo a negocios y manejo de desechos',
+                'Culturales y de esparcimiento, deportivos y recreativos',
+                'Organizaciones con fines altruistas y medio ambientales',
+                'Agricultura, ganadería, aprovechamiento forestal y pesca',
+                'Tecnologías de la información y la comunicación',
+                'Otros',
+                'No sé'
+              ]
+            }
+          },
+          {
+            title    : '¿El producto/servicio que se ofrecerá será?',
+            // help     : 'Selecciona uno de los valores',
+            type     : 'radio',
+            body     : {
+              value    : 'Es nuevo y distinto a lo que existe y/o el proceso de elaboración/comercialización es innovador',
+              options  : ['Es nuevo y distinto a lo que existe y/o el proceso de elaboración/comercialización es innovador', 'En algunas características son diferentes a los de mi competencia o lo ofrezco a personas que no lo tienen', 'Ya existe y es ofrecido por otros' ]
+            }
+          },
+          {
+            title    : '¿Cuál es el nivel de respaldo que dará la tecnología en tu negocio?',
+            // help     : 'Selecciona uno de los valores',
+            type     : 'radio',
+            body     : {
+              value    : 'Ninguna, no se necesitará / no tengo recursos para eso',
+              options  : ['Ninguna, no se necesitará / no tengo recursos para eso', 'Servirá de apoyo para algunos procesos administrativos (procesamiento de nómina, costos, gastos, ventas, etc.)', 'Jugará un papel importante en la empresa (en el proceso de elaboración del producto o servicio que ofrece la empresa, además de apoyar la gestión y administración de la empresa)']
+            }
+          },
+        ]
+      },
+      {
+        identifier : '4.C.1',
+        grouped    : true,
+        questions : [
+          {
+            title  : 'De la siguiente lista selecciona la (o las) prioridad(es) actual(es) que tenga tu empresa. Puedes escoge de 1 a 3 prioridades.',
+            help   : 'Escribe el valor de la respuesta',
+            type   : 'checkbox',
+            body   : {
+              options: [
+                { label: 'Conseguir financiamiento/ Acceder a instrumentos financieros.', checked: true },
+                { label: 'Diseñar o mejorar mi plan de negocios.', checked: true },
+                { label: 'Maximizar la productividad/ Desarrollar procesos.', checked: false },
+                { label: 'Entrar a la formalidad.', checked: false },
+                { label: 'Capacitar al equipo emprendedor/personal de la empresa.', checked: false },
+                { label: 'Expandir mis opciones de mercado.', checked: true },
+                { label: 'Incorporar tecnología e innovación.', checked: true },
+              ]
+            }
+          },
+        ]
+      },
+      {
+        identifier : '5.C.1',
+        grouped    : true,
+        questions : [
+          {
+            title  : 'Las siguientes frases describen diferentes motivos o maneras de iniciar una empresa, elige la opción que más se identifique con tu proyecto.',
+            help   : 'Escribe el valor de la respuesta',
+            type   : 'radio',
+            body   : {
+              value : 'El principal motivo para iniciar mi empresa es tener recursos para cubrir los gastos básicos. Si tuviera la oportunidad buscaría recursos de otra manera.',
+              options: [
+                'El principal motivo para iniciar mi empresa es tener recursos para cubrir los gastos básicos. Si tuviera la oportunidad buscaría recursos de otra manera.',
+                'Mi empresa será similar a muchas otras y mi producto ya se vende, pero aun así puedo obtener ganancias.',
+                'Mi negocio me permitirá hacer lo que más me gusta y ser independiente.',
+                'Lo que quiero es desarrollar mi creatividad y/o expresión artística.',
+                'El objetivo central de mi proyecto es contribuir para mejorar la situación de un grupo de la población y/o el medio ambiente.',
+                'Tengo una idea innovadora con el potencial de ser exitosa y rentable.'
+              ]
+            }
+          },
         ]
       },
 
