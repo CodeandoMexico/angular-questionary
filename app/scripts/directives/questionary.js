6985f20ecb00c2cf393912db8e4d73fbfc7ba5c3
Raúl Jiménez <jimenezjrs@gmail.com>
2014-09-01 16:14:24 -0500

    implemente the design for the multiple choice

--- app/scripts/directives/questionary.js
+++ app/scripts/directives/questionary.js
@@ -12,7 +12,7 @@ app.run(['$templateCache', function($templateCache){
   // answer templates
   $templateCache.put('text-input.html','<input class="form-control" type="text" ng-model="body.value">');
   $templateCache.put('number-input.html','<div ng-class="{ \'has-error\': questionForm.question.$invalid }"><input name="question" class="form-control" ng-required="true" type="number" min="{{body.minimumValue}}" ng-model="body.value"></div>');
-  $templateCache.put('radio-input.html','<div class="radio" ng-repeat="opt in body.options"><label><input type="radio" name="radio{{idx}}" value="{{opt.value}}" ng-model="body.selected_value">{{opt.label}}</label></div>');
+  $templateCache.put('radio-input.html','<div class="question--option" ng-repeat="opt in body.options"><input type="radio" id="radio--job-or-business-{{ idx }}-{{ $index }}" name="radio--job-or-business-{{ outerIdx }}-{{ idx }}" value="{{opt.value}}" ng-model="body.selected_value"><label for="radio--job-or-business-{{ idx }}-{{ $index }}">{{opt.label}}</label></div>');
   $templateCache.put('checkbox-input.html','<div class="checkbox" ng-repeat="opt in body.options"><label><input type="checkbox" name="checkbox{{idx}}" ng-model="opt.checked">{{opt.label}}</label></div>');
   $templateCache.put('select-input.html','<select class="form-control" ng-model="body.selected_value" ng-options="option.label for option in body.options"></select>');
   $templateCache.put('order-input.html','<ol ui-sortable ng-model="body.options" class="order-question"><li ng-repeat="opt in body.options">{{opt.label}}</li></ol>');
