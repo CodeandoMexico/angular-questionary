07a3d27110e3a8a26cb90e9d34e20afd52aab769
Raúl Jiménez <jimenezjrs@gmail.com>
2014-09-24 19:22:54 -0500

    registration and login works correctly

--- app/scripts/services/fondeso-user.js
+++ app/scripts/services/fondeso-user.js
@@ -10,37 +10,30 @@
 angular.module('questionaryApp')
   .service('FondesoUser', ['$http', '$cookies', function fondesoUser($http, $cookies) {
     return {
-      backendAddress: 'http://localhost:3000/users/',
+      backendAddress: 'http://localhost:3000/users',
       current: null,
       validateLogin: function ( data ) {
         return angular.isObject( data );
       },
       userIsLoggedIn: function(){
-        var address = this.backendAddress + 'current/';
+        var address = this.backendAddress + '/current';
         return $http.get(address);
       },
       logIn: function(args){
-        var address = this.backendAddress + 'sign_in';
+        var address = this.backendAddress + '/sign_in';
         console.log('arguments');
         console.log(args);
 
-        // return $http.get(address).
-        // success(function(data, status, headers, config) {
-          var authenticityToken = $cookies['XSRF-TOKEN'];
-          return $http.post(address + '.json', {
-            'authenticity_token': authenticityToken,
-            'user': {
-              'email': args.email,
-              'password': args.password,
-              'remember_me': 0
-            },
-            'commit': 'Sign in'
-          });
-
-        // }).
-        // error(function(data, status, headers, config) {
-        //   console.log('Error: ' + status);
-        // });
+        var authenticityToken = $cookies['XSRF-TOKEN'];
+        return $http.post(address + '.json', {
+          'authenticity_token': authenticityToken,
+          'user': {
+            'email': args.email,
+            'password': args.password,
+            'remember_me': 0
+          },
+          'commit': 'Sign in'
+        });
       },
       logOut: function(){
         return ;
@@ -48,23 +41,16 @@ angular.module('questionaryApp')
       create: function(args){
         var address = this.backendAddress;
 
-        return $http.get(this.backendAddress + 'sign_up/').
-        success(function(data, status, headers, config) {
-          var authenticityToken = $cookies['XSRF-TOKEN'];
-          return $http.post(address, {
-            'authenticity_token': authenticityToken,
-            'user': {
-              'name': args.name,
-              'email': args.email,
-              'password': args.password,
-              'password_confirmation': args.passwordConfirmation
-            },
-              'commit': 'Sign up'
-          });
-
-        }).
-        error(function(data, status, headers, config) {
-          console.log('Error: ' + status);
+        var authenticityToken = $cookies['XSRF-TOKEN'];
+        return $http.post(address + '.json', {
+          'authenticity_token': authenticityToken,
+          'user': {
+            'name': args.name,
+            'email': args.email,
+            'password': args.password,
+            'password_confirmation': args.passwordConfirmation
+          },
+            'commit': 'Sign up'
         });
       },
     };
